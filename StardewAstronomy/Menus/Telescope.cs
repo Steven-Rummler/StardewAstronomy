c070e77a8ad201057f574f1b323468f63eaa86d4
Steven Rummler <steven.rummler@gmail.com>
2024-01-12 10:24:08 -0700

    Learning to draw rectangles

--- StardewAstronomy/Menus/Telescope.cs
+++ StardewAstronomy/Menus/Telescope.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using StardewValley.BellsAndWhistles;
 using StardewValley;
 using StardewValley.Menus;
@@ -12,6 +13,7 @@ namespace StardewAstronomy.Menus
 {
     public class Telescope : IClickableMenu
     {
+        public Texture2D texture;
         public Telescope()
         : base((int)Utility.getTopLeftPositionForCenteringOnScreen(1280, 720).X, (int)Utility.getTopLeftPositionForCenteringOnScreen(1280, 720).Y, 1280, 720, showUpperRightCloseButton: true)
         {
@@ -26,7 +28,7 @@ namespace StardewAstronomy.Menus
             //    myID = 102,
             //    leftNeighborID = 101
             //};
-            //this.letterTexture = Game1.temporaryContent.Load<Texture2D>("LooseSprites\\letterBG");
+            this.texture = Game1.temporaryContent.Load<Texture2D>("LooseSprites\\letterBG");
             //text = this.ApplyCustomFormatting(text);
             //this.mailMessage = SpriteText.getStringBrokenIntoSectionsOfHeight(text, base.width - 64, base.height - 128);
             //this.forwardButton.visible = this.page < this.mailMessage.Count - 1;
@@ -37,6 +39,62 @@ namespace StardewAstronomy.Menus
             {
                 this.snapToDefaultClickableComponent();
             }
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            // Fade out surrounding view
+            b.Draw(
+                Game1.fadeToBlackRect,
+                Game1.graphics.GraphicsDevice.Viewport.Bounds,
+                Color.Black * 0.4f
+            );
+            // Draw background
+            b.Draw(
+                this.texture,
+                new Vector2(base.xPositionOnScreen + base.width / 2, base.yPositionOnScreen + base.height / 2),
+                new Rectangle(0, 0, 320, 180),
+                Color.White,
+                0f,
+                new Vector2(160f, 90f),
+                4f,
+                SpriteEffects.None,
+                0.86f
+            );
+            // Draw a night sky
+            b.Draw(
+                Game1.fadeToBlackRect,
+                new Vector2(base.xPositionOnScreen + base.width / 2, base.yPositionOnScreen + base.height / 2),
+                new Rectangle(0, 0, 100, 100),
+                Color.Black,
+                0f,
+                new Vector2(100f, 50f),
+                4f,
+                SpriteEffects.None,
+                0.85f
+            );
+            Point[] stars =
+            {
+                new Point(0, 0),
+            };
+            // Get current time
+            int time = Game1.timeOfDay / 100;
+            // Each star begins at a point and moves left based on the time of day
+            for ( int i = 0; i < stars.Length; i++ )
+            {
+                b.Draw(
+                    Game1.fadeToBlackRect,
+                    new Vector2(base.xPositionOnScreen + base.width / 2, base.yPositionOnScreen + base.height / 2),
+                new Rectangle(0, 0, 1, 1),
+                Color.White,
+                0f,
+                new Vector2(stars[i].X + time, stars[i].Y),
+                4f,
+                SpriteEffects.None,
+                0.85f
+                    );
+            }
+            
         }
     }
 }
